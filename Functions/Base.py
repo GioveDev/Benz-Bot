0e307bb6dd2be20d82868485a7b15725fb953452
Nicola Le <nicola.le@erowa.com>
2021-07-11 16:50:37 +0200

    Added help slash command

--- Functions/Base.py
+++ Functions/Base.py
@@ -1,4 +1,7 @@
 from discord.ext import commands
+from discord_slash import cog_ext
+
+description_string = "Ich lutsche Ihren Schwanz und spiele Musik \n-**join** <channel name> \n-**play** <local file name> \n-**stream** <video link> \n-**volume** <0-100> \n-**stop** \n-**Herr Benz** wenn sie mit mir reden wollen"
 
 class Base(commands.Cog):
     @commands.Cog.listener()
@@ -6,9 +9,13 @@ class Base(commands.Cog):
         print(f'Succesfully logged in!')
         print('------')
         
-    @commands.command()
-    async def help(self, ctx):
-        await ctx.send("Ich lutsche Ihren Schwanz und spiele Musik \n-**join** <channel name> \n-**play** <local file name> \n-**stream** <video link> \n-**volume** <0-100> \n-**stop** \n-**Herr Benz** wenn sie mit mir reden wollen")
+    @commands.command(name = "help")
+    async def command_help(self, ctx):
+        await ctx.send(description_string)
+
+    @cog_ext.cog_slash(name = "help", description = "fucking help me pls")
+    async def slash_help(self, ctx):
+        await ctx.send(description_string)
             
 def setup(bot):
     bot.add_cog(Base(bot))
