0ecf248c211d63df1f4e5460b4989db6c80c62dc
Nicola Le <nicola.le@erowa.com>
2021-07-26 20:47:49 +0200

    Completed Cleverbot Chat implementation

--- Functions/CleverBotChat/CleverBotChat.py
+++ Functions/CleverBotChat/CleverBotChat.py
@@ -1,17 +1,29 @@
 import asyncio
+from json.decoder import JSONDecodeError
 import cleverbotfree
 from discord.ext import commands
+
+import json
+import requests
+
+#Honestly this is fucking jank but it works
 
 class CleverBotChat(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
+        if message.content.startswith('Herr Benz '):
             message_string = message.content[10:]
-            if message.content.startswith('Herr Benz '):
-                async with cleverbotfree.async_playwright() as p_w:
-                    async with message.channel.typing():
-                        c_b = await cleverbotfree.CleverbotAsync(p_w)
-                        bot = await c_b.single_exchange(message_string)
-                        await message.channel.send(bot)
+
+            try:
+                async with message.channel.typing():
+                    reply = requests.get("http://192.168.1.30:5000/chat", params = { 'message':message_string })
+                    reply_text = json.loads(reply.text)
+            except JSONDecodeError:
+                await message.channel.send("An error has occurred, if this issue persists contact my master")
+            except requests.ConnectionError:
+                await message.channel.send("An error has occurred, if this issue persists contact my master")
+
+            await message.channel.send(reply_text)
 
 def setup(bot):
     bot.add_cog(CleverBotChat(bot))
--- Functions/Talk.py
+++ Functions/Talk.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 class Talk(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.content.startswith('Herr Benz2 '):
+        if message.content.startswith('Secret '):
             await message.channel.send('Ja, aber zuerst müssen sie die Projektdokumentation schreiben')
 
 def setup(bot):
